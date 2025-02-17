18f056d72acf8d1403115759c2472ab5075b681e
Mrneptune11 <sebastianlanz301@gmail.com>
2025-02-16 23:43:16 -0500

    set up

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+      <div>
+        <h1>Shopping List Manager</h1>
+        <p id="welcome">Welcome to shopping list manager! Please create login to begin</p>
+        <form id="loginform">
+          <label htmlFor="username"></label>
+          <input type="text" id="username" placeholder="Username"/>
+          <br/>
+          <label htmlFor="password"></label>
+          <input type="text" id="password" placeholder="Password"/>
+          <br/>
+          <button id="loginbutton">log in</button>
+          <button id="signupbutton">sign up</button>
+        </form>
+      </div>
+  );
+}
+
+export default App;
